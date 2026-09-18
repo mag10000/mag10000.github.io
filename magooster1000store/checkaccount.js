80ebb3ad85485f6a04672ddfd8f7160a981c03d9
Magooster1000 <145853867+mag10000@users.noreply.github.com>
2026-09-17 19:15:55 -0500

    Update checkaccount.js

--- magooster1000store/checkaccount.js
+++ magooster1000store/checkaccount.js
@@ -1,1 +1,6 @@
-
+var app_div = document.getElementById("AppDiv");
+app_div.style.display = "none";
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const token = urlParams.get('token')
+console.log("Token: " + token)

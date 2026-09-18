6fc76a8ea990d287d8f9025081271cb29047507a
Magooster1000 <145853867+mag10000@users.noreply.github.com>
2026-09-18 13:58:53 -0700

    Delete magooster1000store/checkaccount.js

--- magooster1000store/checkaccount.js
+++
@@ -1,6 +0,0 @@
-var app_div = document.getElementById("AppDiv");
-app_div.style.display = "none";
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const token = urlParams.get('token')
-console.log("Token: " + token)
